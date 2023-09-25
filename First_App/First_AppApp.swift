a526713bf4bad4441be0218c3e471fe5a9c28f52
Your Name <kyrstn.hall@gmail.com>
2023-09-25 00:18:24 -0700

    Initial Commit

---
+++ First_App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First_App
+//
+//  Created by Kyrstn Hall on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
